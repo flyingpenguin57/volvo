83eeeacd65d5548e36fc0ba4c5832d54748bf0d3
hulio <597314452@qq.com>
2025-05-30 15:03:17 +0800

    test commit

--- src/main/java/com/example/volvo/controller/AccountsController.java
+++ src/main/java/com/example/volvo/controller/AccountsController.java
@@ -25,6 +25,7 @@ public class AccountsController {
     public CommonResponse<?> updateAccount(@Valid @RequestBody UpdateAccountRequest updateAccountRequest) {
         accountsService.updateAccount(updateAccountRequest.getEmail(), updateAccountRequest.getStatus());
         return CommonResponse.ok();
+
     }
 
     @GetMapping("/queryAccount")
